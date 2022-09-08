e9de2dc4bbd23b60d2c31b72565bb399b6f370bb
MaraOD555 <ruffbc@hotmail.com>
2022-09-08 23:00:01 +0300

    дз

--- src/main/java/pro/sky/HW16algorithms3/IntegerListImpl.java
+++ src/main/java/pro/sky/HW16algorithms3/IntegerListImpl.java
@@ -1,13 +1,12 @@
 package pro.sky.HW16algorithms3;
 
-import pro.sky.HW16algorithms3.exception.FullRepositoryException;
 import pro.sky.HW16algorithms3.exception.InvalidIndexException;
 import pro.sky.HW16algorithms3.exception.NullItemException;
 
 import java.util.Arrays;
 
 public class IntegerListImpl implements IntegerList {
-    private final Integer[] repository;
+    private Integer[] repository;
     private int size;
 
     public IntegerListImpl() {
@@ -20,18 +19,22 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Integer add(Integer item) {
-        checkSize();
         checkItem(item);
+        if (size == repository.length) {
+            grow();
+        }
         repository[size++] = item;
         return item;
     }
 
     @Override
     public Integer add(int index, Integer item) {
-        checkSize();
         checkItem(item);
         checkIndex(index);
-        if (index == size){
+        if (size == repository.length) {
+            grow();
+        }
+        if (index == size) {
             repository[size++] = item;
             return item;
         }
@@ -60,7 +63,7 @@ public class IntegerListImpl implements IntegerList {
     public Integer remove(int index) {
         checkIndex(index);
         Integer item = repository[index];
-        if(index != size) {
+        if (index != size) {
             System.arraycopy(repository, index + 1, repository, index, size - index); // сдвиг влево
         }
         size--;
@@ -71,7 +74,7 @@ public class IntegerListImpl implements IntegerList {
     public boolean contains(Integer item) {
         checkItem(item);
         Integer[] repositoryCopy = toArray(); // toArray дает копию массива, поэтому работа идет с новым массивом
-        sortInsertion(repositoryCopy); // сортировка
+        quickSort(repositoryCopy, 0, repositoryCopy.length - 1); // сортировка
         int min = 0; // метод бинарного поиска
         int max = repositoryCopy.length - 1;
         while (min <= max) {
@@ -82,7 +85,7 @@ public class IntegerListImpl implements IntegerList {
             if (item < repositoryCopy[mid]) {
                 max = mid - 1;
             } else {
-                min = mid +1;
+                min = mid + 1;
             }
         }
         return indexOf(item) != -1;
@@ -91,7 +94,7 @@ public class IntegerListImpl implements IntegerList {
     @Override
     public int indexOf(Integer item) {
         for (int i = 0; i < size; i++) {
-            if(repository[i].equals(item)){
+            if (repository[i].equals(item)) {
                 return i;
             }
         }
@@ -100,7 +103,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public int lastIndexOf(Integer item) {
-        for (int i = size-1; i >= 0; i--) {
+        for (int i = size - 1; i >= 0; i--) {
             if (repository[i].equals(item)) {
                 return i;
             }
@@ -117,7 +120,7 @@ public class IntegerListImpl implements IntegerList {
     @Override
     public boolean equals(IntegerList otherList) {
 
-        return Arrays.equals(this.toArray(),otherList.toArray());
+        return Arrays.equals(this.toArray(), otherList.toArray());
     }
 
     @Override
@@ -140,21 +143,18 @@ public class IntegerListImpl implements IntegerList {
         return Arrays.copyOf(repository, size);
     }
 
-    private void checkItem (Integer item){
-        if (item == null){
+    private void checkItem(Integer item) {
+        if (item == null) {
             throw new NullItemException();
         }
     }
-    private void checkSize (){
-        if (size == repository.length){
-            throw new FullRepositoryException();
-        }
-    }
-    private void checkIndex (int index){
-        if (index < 0 || index > size){
+
+    private void checkIndex(int index) {
+        if (index < 0 || index > size) {
             throw new InvalidIndexException();
         }
     }
+
     private void sortInsertion(Integer[] arr) { //самый быстрый способ сортировки
         for (int i = 1; i < arr.length; i++) {
             int temp = arr[i];
@@ -166,4 +166,42 @@ public class IntegerListImpl implements IntegerList {
             arr[j] = temp;
         }
     }
+        private void quickSort ( Integer[] arr, int begin, int end){ // быстрая сортировка с рекурсией
+            if (begin < end) {
+                int partitionIndex = partition(arr, begin, end);
+
+                quickSort(arr, begin, partitionIndex - 1);
+                quickSort(arr, partitionIndex + 1, end);
+            }
+        }
+        private int partition (Integer[] arr, int begin, int end){
+            int pivot = arr[end];
+            int i = (begin - 1);
+
+            for (int j = begin; j < end; j++) {
+                if (arr[j] <= pivot) {
+                    i++;
+
+                    swapElements(arr, i, j);
+                }
+            }
+            swapElements(arr, i + 1, end);
+            return i + 1;
+        }
+        private static void swapElements (Integer[] arr, int left, int right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
+
+        /* private void grow () { // массив растет в полтора раза, становится 15 элементов
+            size = size + size/2;
+            Integer[] newRepository = new Integer[size];
+            Arrays.copyOf(repository, size);
+            repository = newRepository;
+        } */
+        private void grow() {
+            repository = Arrays.copyOf(repository, size + size/2);
+        }
+
 }
